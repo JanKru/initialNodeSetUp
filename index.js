f9084fadb735d558d00ad8e5688c3d7fcd90afa2
Jan Kruse <jankruse@WS0219.local>
2019-06-04 14:12:25 +0200

    Added logging

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const config = require('./config/config');
 const app = require('./config/express');
+const logger = require('./config/log');
 
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   app.listen(config.port, () => {
-    console.info(`server started on port ${config.port} (${config.env})`);
+    logger.info(`server started on port ${config.port} (${config.env})`);
   });
 }
 
